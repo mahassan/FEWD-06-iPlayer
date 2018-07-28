91ed5134e6568227e732def383af7600bb844e43
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-07-28 21:44:31 +0100

    sync captions with video and some glitters

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 const video = document.getElementsByTagName('VIDEO')[0];
 const span = document.querySelectorAll('p span');
-video.ontimeupdate = function(){
-    for(i = 0; i < span.length;i++){
+
+// add features from the API
+$('video, audio').mediaelementplayer({
+    features: ['playpause', 'fullscreen', 'volume', 'progress'],
+    startLanguage: 'en'
+});
+
+video.ontimeupdate = function () {
+    for (i = 0; i < span.length; i++) {
         const timeSeek = video.currentTime;
-        const toDecimal = timeSeek.toFixed(3);
-        if(toDecimal >= span[i].getAttribute("data-start") && toDecimal <= span[i].getAttribute("data-end")){
-            span[i].style.color = 'red';
-            console.log(toDecimal);
-        }else{
-            span[i].style.color = 'black';
+        //.toFixed(3);
+        if (timeSeek > span[i].getAttribute("data-start") && timeSeek < span[i].getAttribute("data-end")) {
+            span[i].style.backgroundColor = '#1B3440';
+            span[i].style.color = '#fff';
+        } else {
+            span[i].style.color = '#000';
+            span[i].style.backgroundColor = '';
         }
 
     }
