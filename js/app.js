df9f4d3217240fe35761997db73cee44aad5b6a2
Ammar Hassan <ammar@ammarhassan.co.uk>
2018-07-29 01:23:00 +0100

    JSLinted

--- js/app.js
+++ js/app.js
@@ -1,30 +1,31 @@
-const video = document.getElementsByTagName('VIDEO')[0];
-const span = document.querySelectorAll('p span');
+const video = document.getElementsByTagName("VIDEO")[0];
+const span = document.querySelectorAll("p span");
 
 // add features from the API
-$('video, audio').mediaelementplayer({
+$("video, audio").mediaelementplayer({
     useDefaultControls: true,
-    startLanguage: 'en'
+    startLanguage: "en"
 });
 
 video.ontimeupdate = function () {
-    for (i = 0; i < span.length; i++) {
+    for(i = 0; i < span.length; i+=1) {
         const timeSeek = video.currentTime;
         //.toFixed(3);
-        if (timeSeek > span[i].getAttribute("data-start") && timeSeek < span[i].getAttribute("data-end")) {
-            span[i].style.backgroundColor = '#1B3440';
-            span[i].style.color = '#fff';
+        if (timeSeek > span[i].getAttribute("data-start") 
+          && timeSeek < span[i].getAttribute("data-end")) {
+            span[i].style.backgroundColor = "#1B3440";
+            span[i].style.color = "#fff";
         } else {
-            span[i].style.color = '#000';
-            span[i].style.backgroundColor = '';
+            span[i].style.color = "#000";
+            span[i].style.backgroundColor = "";
         }
 
     }
 }
-const spanOne = document.querySelectorAll('p span');
-for(let i = 0; i <spanOne.length;i++ ){
-    spanOne[i].addEventListener('click', function(event){
+const spanOne = document.querySelectorAll("p span");
+for(let i = 0; i <spanOne.length;i+=1 ){
+    spanOne[i].addEventListener("click", function(event){
         //console.log(event.target);
-        video.currentTime = event.target.getAttribute('data-start');
+        video.currentTime = event.target.getAttribute("data-start");
     }) 
 }
